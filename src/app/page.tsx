f5312617cd7ff4a2316eb8a3fc2d5bbb867c58f3
zul-grl <zulgerel06n@gmail.com>
2025-06-25 16:18:01 +0800

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -197,7 +197,7 @@ export default function Home() {
             </CardContent>
           </Card>
           <CloudinaryUpload handleFile={handleFile} />
-          <div className="flex justify-end">
+          <div className="flex justify-end mt-4">
             <Button
               type="submit"
               size="lg"
